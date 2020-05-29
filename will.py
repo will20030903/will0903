90f2cc9f9435bd91fb31e594368fc0de53cabe07
will0903 <anin0919@gmail.com>
2020-05-29 19:28:27 +0800

    0529

--- will.py
+++ will.py
@@ -1,5 +1,12 @@
-# for 迴圈
-sum=1
-for x in range(1,11):
-    sum=sum*x
-print(sum)
+score = int(input('請輸入考試分數 (1-100)：'))
+
+if score > 100:
+    print('請勿亂輸入！')
+elif score >= 90 and score <= 100:
+    print('成績：A+')
+elif score >= 80 and score < 90:
+    print('成績：B+')
+elif score >= 70 and score < 80:
+    print('成績：B')
+else:
+    print('成績：C')
